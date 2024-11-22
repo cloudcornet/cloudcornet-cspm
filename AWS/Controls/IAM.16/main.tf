96a260a2ba6ddea4b0c60b8fca1e87c391c43ccb
cloudcornet <regis.lecornet@cloudcornet.com>
2024-11-22 18:08:51 +0100

    Remove directory IAM.16

--- AWS/Controls/IAM.16/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_iam_account_password_policy" "this" {
-  # Enforce password reuse prevention
-  password_reuse_prevention  = var.aws_password_reuse_prevention # Set the number of previous passwords to remember (1-24)
-}
